c6121cc083f02e1beff3528915ccf7e76ce51c16
etorreborre <etorreborre@yahoo.com>
2022-12-20 18:41:02 +0100

    refactor: used the ambassador crate to delegate the AuthenticatedStorage implementation

--- implementations/rust/ockam/ockam_identity/src/authenticated_storage.rs
+++ implementations/rust/ockam/ockam_identity/src/authenticated_storage.rs
@@ -1,9 +1,12 @@
 use ockam_core::async_trait;
 use ockam_core::compat::{boxed::Box, string::String, vec::Vec};
 use ockam_core::{AsyncTryClone, Result};
+use ambassador::delegatable_trait;
+use ambassador::Delegate;
 
 /// Storage for Authenticated data
 #[async_trait]
+#[delegatable_trait]
 pub trait AuthenticatedStorage: AsyncTryClone + Send + Sync + 'static {
     /// Get entry
     async fn get(&self, id: &str, key: &str) -> Result<Option<Vec<u8>>>;
@@ -24,36 +27,10 @@ pub mod database;
 use mem::*;
 use ockam_node::database::lmdb::*;
 
+#[derive(Delegate)]
+#[delegate(AuthenticatedStorage)]
 #[derive(Clone, Debug)]
 pub enum AuthenticatedStorageImpl {
   DatabaseImpl(LmdbStorage),
   InMemoryImpl(InMemoryStorage)
-}
-use AuthenticatedStorageImpl::*;
-
-#[async_trait]
-impl AuthenticatedStorage for AuthenticatedStorageImpl {
-    /// Get entry
-    async fn get(&self, id: &str, key: &str) -> Result<Option<Vec<u8>>> {
-        match self {
-            DatabaseImpl(s) => (*s).get(id, key).await,
-            InMemoryImpl(s) => (*s).get(id, key).await
-        }
-    }
-
-    /// Set entry
-    async fn set(&self, id: &str, key: String, val: Vec<u8>) -> Result<()> {
-        match self {
-            DatabaseImpl(s) => s.set(id, key, val).await,
-            InMemoryImpl(s) => s.set(id, key, val).await
-        }
-    }
-
-    /// Delete entry
-    async fn del(&self, id: &str, key: &str) -> Result<()> {
-        match self {
-            DatabaseImpl(s) => s.del(id, key).await,
-            InMemoryImpl(s) => s.del(id, key).await
-        }
-    }
 }
